c7a59d616df3608d99dc40aa16f773ea270a5979
brunoborges2026 <brunoborgesdossantos2024@gmail.com>
2026-09-05 00:17:16 -0300

    Update Google Apps Script URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Cole a URL do seu Google Apps Script aqui.
 // Deve terminar com /exec
-const CONFIG_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxE_6f-3vwVS4pKZ48pDrLFM4V65xkRIen0dhb_o-7N7gyQCP0cRcwpn9DzI612ciTKig/exec";
+const CONFIG_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwvmte4eJvSpG2CdS-d0JVFOHBgpRjgH8XH1WJEC2aoQhN6CM8QwGHwsOeFg7KU-3dhkg/exec";
 
 // Helper universal para evitar bloqueios CORS de Preflight no Google Apps Script
 async function apiCall(payload) {
